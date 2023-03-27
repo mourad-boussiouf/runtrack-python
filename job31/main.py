b08e11bc6b132100a9d635440c08d4017434bf28
mourad-boussiouf <mourad.boussiouf@laplateforme.io>
2023-03-27 20:54:03 +0200

    la string est iteré, si un caractère non alphébétique est trouvé le script exit, bon début

--- job31/main.py
+++ job31/main.py
@@ -3,7 +3,12 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 
 
 def function(arg):
-    print(arg)
+    for char in arg:
+        if char.lower() in alphabet:
+            print(char.lower(), "dans l'array alphabet")
+        else:
+            print(char.lower(), "Votre input contient des caractères non alphabétiques ou spéciaux")
+            return
 
 user_input = input("Entez votre input: ")
 function(user_input)
