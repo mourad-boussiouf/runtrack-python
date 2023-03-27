3689341b074e98fe504c5b1e578c5d6a08c1dfaa
mourad-boussiouf <mourad.boussiouf@laplateforme.io>
2023-03-27 21:03:23 +0200

    le programme cherche a chaque caractere , si le char est dans l'aphabet array, sa position dans cet array

--- job31/main.py
+++ job31/main.py
@@ -1,13 +1,12 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-
-
 def function(arg):
     for char in arg:
         if char.lower() in alphabet:
-            print(char.lower(), "dans l'array alphabet")
+            index = alphabet.index(char.lower()) + 1
+            print(char.lower(), "Est dans l'alphabet à la position: ", index)
         else:
-            print(char.lower(), "Votre input contient des caractères non alphabétiques ou spéciaux")
+            print(char.lower(), "Votre string contient un caractère qui n'est pas dans l'aphabet, le script va s'arrêter.")
             return
 
 user_input = input("Entez votre input: ")
