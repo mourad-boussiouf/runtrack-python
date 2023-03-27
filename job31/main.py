9c9d00ffe274e57597946b76052ce5a5b8673b83
mourad-boussiouf <mourad.boussiouf@laplateforme.io>
2023-03-27 20:42:55 +0200

    ajout demande user input

--- job31/main.py
+++ job31/main.py
@@ -1,4 +1,9 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
+
+
 def function(arg):
     print(arg)
+
+user_input = input("Entez votre input: ")
+function(user_input)
