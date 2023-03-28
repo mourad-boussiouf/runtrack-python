0392e15dac9d7b67031470dea8186fda5935cfac
mourad-boussiouf <mourad.boussiouf@laplateforme.io>
2023-03-28 15:37:05 +0200

    print new arg , gestion edge case à faire

--- job31/main.py
+++ job31/main.py
@@ -1,13 +1,16 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 def function(arg):
+    Newarg=''
     for char in arg:
         if char.lower() in alphabet:
             index = alphabet.index(char.lower()) + 1
-            print(char.lower(), "Est dans l'alphabet à la position: ", index)
+            Newarg=Newarg+alphabet[index-1]
+            print(char.lower(), "Est dans l'alphabet à la position: ", index,alphabet[index-1])
         else:
-            print(char.lower(), "Votre string contient un caractère qui n'est pas dans l'aphabet, le script va s'arrêter.")
+            print(char.lower(), "Votre mot contient un caractère qui n'est pas dans l'aphabet, le script va s'arrêter.")
             return
+    print(Newarg)
 
 user_input = input("Entez votre input: ")
 function(user_input)
