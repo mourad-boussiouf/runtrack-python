6cea5fd5a009901c24defa33d911eaa10ac34a4f
mourad-boussiouf <mourad.boussiouf@laplateforme.io>
2023-03-28 15:50:52 +0200

    la derniere lettre du mot est remplacé par une lettre plus loin dans l'alphabet. si le mot finit par un Z il faut gérer le edge case

--- job31/main.py
+++ job31/main.py
@@ -2,15 +2,20 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 
 def function(arg):
     Newarg=''
+    NewMot=''
+    count = 1
     for char in arg:
         if char.lower() in alphabet:
+            count=count+1
             index = alphabet.index(char.lower()) + 1
             Newarg=Newarg+alphabet[index-1]
-            print(char.lower(), "Est dans l'alphabet à la position: ", index,alphabet[index-1])
+            print(char.lower(), "Est dans l'alphabet à la position: ", index,alphabet[index-1],count)
+            if count == len(arg):
+                print("dernier charactère",Newarg+alphabet[index+1])
         else:
             print(char.lower(), "Votre mot contient un caractère qui n'est pas dans l'aphabet, le script va s'arrêter.")
             return
-    print(Newarg)
+    print(Newarg, len(Newarg))
 
 user_input = input("Entez votre input: ")
 function(user_input)
