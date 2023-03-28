c234462e2f3681cc147632178e175e7ef33303a2
mourad-boussiouf <mourad.boussiouf@laplateforme.io>
2023-03-28 16:06:40 +0200

    toto donne totp eywa

--- job31/main.py
+++ job31/main.py
@@ -1,19 +1,22 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
+
 def function(arg):
     Newarg=''
     NewMot=''
-    count = 1
+    count = 0
+    
     for char in arg:
         if char.lower() in alphabet:
             count=count+1
             index = alphabet.index(char.lower()) + 1
+            
+            if count == len(arg):
+                print("dernier charactère",Newarg+alphabet[index])
             Newarg=Newarg+alphabet[index-1]
             print(char.lower(), "Est dans l'alphabet à la position: ", index,alphabet[index-1],count)
-            if count == len(arg):
-                print("dernier charactère",Newarg+alphabet[index+1])
         else:
-            print(char.lower(), "Votre mot contient un caractère qui n'est pas dans l'aphabet, le script va s'arrêter.")
+            print(char.lower(), "Votre mot contient un caractère qui n'est pas dans l'alphabet, le script va s'arrêter.")
             return
     print(Newarg, len(Newarg))
 
