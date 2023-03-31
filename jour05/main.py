d75c47768ccf7c717ab8c5c49328aa7efb738ec2
mourad-boussiouf <mourad.boussiouf@laplateforme.io>
2023-03-31 18:36:09 +0200

    oubli argument dans l'instanciaton de la classe TicTacToe

--- jour05/main.py
+++ jour05/main.py
@@ -4,9 +4,11 @@ from random import randint
 from pygame.locals import *
 
 WINDOW_SIZE = 600
+CELL_SIZE = WINDOW_SIZE // 3
 
 class TicTacToe:
-    def __init__(self):
+    def __init__(self, game):
+        self.game = game
         self.field_image = self.get_scaled_image(path='resources/field.png', res=[WINDOW_SIZE]*2)
 
     def draw(self, surface):
@@ -22,7 +24,7 @@ class Game:
         pg.init()
         self.screen = pg.display.set_mode([WINDOW_SIZE]*2)
         self.clock = pg.time.Clock()
-        self.tictactoe = TicTacToe()
+        self.tictactoe = TicTacToe(self)
 
     def check_events(self):
         for event in pg.event.get():
@@ -33,8 +35,8 @@ class Game:
     def run(self):
         while True:
             self.check_events()
-            self.screen.fill((255, 255, 255))
-            self.tictactoe.draw(self.screen)
+            self.screen.fill((255, 255, 255)) #b
+            self.tictactoe.draw(self.screen,) #b
             pg.display.update()
             self.clock.tick(60)
 
