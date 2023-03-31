9d68bc65fdc5e354dbd1b4b92cd14ffcfe3f89e5
mourad-boussiouf <mourad.boussiouf@laplateforme.io>
2023-03-31 11:26:00 +0200

    l'arrière plan s'affiche

--- jour05/main.py
+++ jour05/main.py
@@ -2,31 +2,42 @@ import pygame as pg
 import sys
 from random import randint
 from pygame.locals import *
+
 WINDOW_SIZE = 600
+
+class TicTacToe:
+    def __init__(self):
+        self.field_image = self.get_scaled_image(path='resources/field.png', res=[WINDOW_SIZE]*2)
+
+    def draw(self, surface):
+        surface.blit(self.field_image, (0, 0))
+
+    @staticmethod
+    def get_scaled_image(path, res):
+        img=pg.image.load(path)
+        return pg.transform.scale(img, res)
 
 class Game:
     def __init__(self):
         pg.init()
-        self.TicTacToe1337 = pg.display.set_mode([WINDOW_SIZE]*2)
+        self.screen = pg.display.set_mode([WINDOW_SIZE]*2)
         self.clock = pg.time.Clock()
+        self.tictactoe = TicTacToe()
 
     def check_events(self):
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 pg.quit()
                 sys.exit()
+
     def run(self):
         while True:
             self.check_events()
-            self.clock.tick(60)
+            self.screen.fill((255, 255, 255))
+            self.tictactoe.draw(self.screen)
             pg.display.update()
-
+            self.clock.tick(60)
 
 if __name__=='__main__':
     game = Game()
     game.run()
-
-
-
-
-
