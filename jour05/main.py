b48ba608ef22f241fca2b1edd01fe20e107a03c6
mourad-boussiouf <mourad.boussiouf@laplateforme.io>
2023-03-31 10:54:29 +0200

    classes OK, le jeu peut se lancer et la fenetre s'affiche

--- jour05/main.py
+++ jour05/main.py
@@ -7,7 +7,7 @@ WINDOW_SIZE = 600
 class Game:
     def __init__(self):
         pg.init()
-        self.screen = pg.display.set_mode([WINDOW_SIZE]*2)
+        self.TicTacToe1337 = pg.display.set_mode([WINDOW_SIZE]*2)
         self.clock = pg.time.Clock()
 
     def check_events(self):
@@ -15,13 +15,18 @@ class Game:
             if event.type == pg.QUIT:
                 pg.quit()
                 sys.exit()
+    def run(self):
+        while True:
+            self.check_events()
+            self.clock.tick(60)
+            pg.display.update()
+
+
+if __name__=='__main__':
+    game = Game()
+    game.run()
 
 
 
 
 
-
-
-	
-TicTacToe1337 = pygame.display.set_mode((600,600))
-print(TicTacToe1337)
